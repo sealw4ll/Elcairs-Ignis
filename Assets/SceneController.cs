500dcb3bd0f31331c9ed2ee903906fc9ab20e36c
SummerNatsuha (Ethan) <148058160+natsuha-etoile@users.noreply.github.com>
2024-11-13 02:25:08 +0800

    UIUX ok

---
+++ Assets/SceneController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //use SceneController.instance.NextLevel(); to go to next level in the call func
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
+    }
+
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}

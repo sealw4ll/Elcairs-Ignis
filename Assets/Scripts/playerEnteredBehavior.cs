acc2d5b8fa4024406d418feb97fc148969efe225
schu0089 <schu0089@student.monash.edu>
2024-11-12 21:45:52 +0800

    turret is now LOS

--- Assets/Scripts/playerEnteredBehavior.cs
+++ Assets/Scripts/playerEnteredBehavior.cs
@@ -1,14 +1,18 @@
+using System.Diagnostics.Contracts;
 using UnityEngine;
 
 public class playerEnteredBehavior : MonoBehaviour
 {
     private Transform target;
     public vomitProjectile gun;
-    public bool playerWithin = false;
+    private bool playerWithin = false;
+    private bool los = false;
+    public LayerMask stageMask;
 
     public void setTarget(Transform target)
     {
         this.target = target;
+        this.los = false;
     }
 
     public void Update()
@@ -17,18 +21,34 @@ public class playerEnteredBehavior : MonoBehaviour
         {
             Vector2 direction = -1 * (target.position - transform.position);
             transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+
+            if (Physics2D.Linecast(target.position, transform.position, stageMask))
+                noLOS();
+            else
+                HaveLOS();
         }
+    }
+
+    public void HaveLOS()
+    {
+        this.los = true;
+        gun.started = true;
+    }
+
+    public void noLOS()
+    {
+        this.los = false;
+        gun.started = false;
     }
 
     public void playerEntered()
     {
         playerWithin = true;
-        gun.started = true;
     }
 
     public void playerExit()
     {
         playerWithin = false;
-        gun.started = false;
+        noLOS();
     }
 }

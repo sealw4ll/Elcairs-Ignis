59ae4a894f1496d231126d0ff5990e20a63d4eb5
Ivan(Laptop) <ivan.teohjh@gmail.com>
2024-11-13 18:58:44 +0800

    checkpoint and killzone scripts

---
+++ Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject respawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}

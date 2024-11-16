f9702ab46f30489b06c4158a032fd33e8d32b2e5
schu0089 <schu0089@student.monash.edu>
2024-11-16 18:27:27 +0800

    fixed collision

--- Assets/Scripts/AttackArea.cs
+++ Assets/Scripts/AttackArea.cs
@@ -5,6 +5,27 @@ public class AttackArea : MonoBehaviour
     public int damage = 3;
     public string targetTag;
 
+    public bool targetWithin = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == targetTag && collision.GetComponent<Health>() != null && !targetWithin)
+        {
+            Health targetHP = collision.GetComponent<Health>();
+            targetHP.dealDmg(damage, this.GetComponent<Rigidbody2D>());
+            targetWithin = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == targetTag && targetWithin)
+        {
+            targetWithin = false;
+        }
+    }
+
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == targetTag && collision.GetComponent<Health>() != null)
@@ -13,4 +34,5 @@ public class AttackArea : MonoBehaviour
             targetHP.dealDmg(damage, this.GetComponent<Rigidbody2D>());
         }
     }
+    */
 }
--- Assets/Scripts/bulletBehavior.cs
+++ Assets/Scripts/bulletBehavior.cs
@@ -5,7 +5,7 @@ public class bulletBehavior : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         // player HITBOX and walls
         if (collision.gameObject.layer == 6 || 
@@ -15,10 +15,6 @@ public class bulletBehavior : MonoBehaviour
 
         if (collision.gameObject.tag == "PlayerAttack")
             SceneController.instance.AudioManager.PlaySFX(SceneController.instance.AudioManager.projectile_clash);
-    }
-
-    private void Start()
-    {
     }
 
     public void setDirection(Vector3 dir)
